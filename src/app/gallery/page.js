99e9a91be1d060e0bda9740ca85b008ef130f604
shivam.goyal <shivam.goyal@squareyards.co.in>
2024-09-19 16:08:28 +0530

    more changes done

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -2,9 +2,9 @@ function page() {
   return (
    <>
 
-<div class="galleryMainBanner">
-            <img src="images/galleryMainBanner.png" class="galleryMainBg img-responsive" width="100" height="776"/>
-            <div class="bannerContent">
+<div className="galleryMainBanner">
+            <img src="images/galleryMainBanner.png" className="galleryMainBg img-responsive" width="100" height="776"/>
+            <div className="bannerContent">
                 <figure>
                     <img src="images/crown.png"/>
                 </figure>
@@ -13,113 +13,113 @@ function page() {
                 <h3>Your Dream Wedding Starts Here !</h3>
             </div>
         </div>
-        <div class="container">
-        <div class="imageGalleryBox">
+        <div className="container">
+        <div className="imageGalleryBox">
             
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery1.png " class="img-responsive" />
+                    <img src="images/gallery1.png " className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery2.png" class="img-responsive" />
+                    <img src="images/gallery2.png" className="img-responsive" />
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery3.png" class="img-responsive" />
+                    <img src="images/gallery3.png" className="img-responsive" />
                     
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery4.png" class="img-responsive" />
+                    <img src="images/gallery4.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery5.png" class="img-responsive" />
+                    <img src="images/gallery5.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery6.png" class="img-responsive" />
+                    <img src="images/gallery6.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery7.png" class="img-responsive" />
+                    <img src="images/gallery7.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery8.png" class="img-responsive" />
+                    <img src="images/gallery8.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery9.png" class="img-responsive" />
+                    <img src="images/gallery9.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery10.png" class="img-responsive" />
+                    <img src="images/gallery10.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery11.png" class="img-responsive" />
+                    <img src="images/gallery11.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery12.png" class="img-responsive" />
+                    <img src="images/gallery12.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery13.png" class="img-responsive" />
+                    <img src="images/gallery13.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery14.png" class="img-responsive" />
+                    <img src="images/gallery14.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery15.png" class="img-responsive" />
+                    <img src="images/gallery15.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery16.png" class="img-responsive" />
+                    <img src="images/gallery16.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery17.png" class="img-responsive" />
+                    <img src="images/gallery17.png" className="img-responsive" />
 
                 </figure>
             </div>
-            <div class="galleryCard">
+            <div className="galleryCard">
                 <figure>
-                    <img src="static/assets/images/gallery18.png" class="img-responsive" />
+                    <img src="images/gallery18.png" className="img-responsive" />
 
                 </figure>
             </div>
